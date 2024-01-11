7efded9bf20f03333606fd025dd170f282b14573
Denis Kuznetsov <denis.kyznecov@gmail.com>
2024-01-11 16:05:14 +0300

    1

--- src/app/screen/screen.component.ts
+++ src/app/screen/screen.component.ts
@@ -11,10 +11,9 @@ import Konva from 'konva';
 export class ScreenComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     let div = document.querySelector('div');
-    console.log(div);
     if (div) {
-      var width = window.innerWidth;
-      var height = window.innerHeight;
+      let width = window.innerWidth;
+      let height = window.innerHeight;
       // создали сцену, указали контейнер для сцены
       var stage = new Konva.Stage({
         container: div,
@@ -23,39 +22,28 @@ export class ScreenComponent implements AfterViewInit {
       });
       // создали слой
       var layer = new Konva.Layer();
-      // создали цветной квадрат
-      var rect1 = new Konva.Rect({
-        x: 20,
-        y: 20,
-        width: 100,
-        height: 50,
-        fill: 'green',
-        stroke: 'black',
-        strokeWidth: 4,
-      });
-      // add the shape to the layer
-      layer.add(rect1);
-      //
-      var rect2 = new Konva.Rect({
-        x: 150,
-        y: 40,
-        width: 100,
-        height: 50,
-        fill: 'red',
-        shadowBlur: 10,
-        cornerRadius: 10,
-      });
-      layer.add(rect2);
 
-      var rect3 = new Konva.Rect({
-        x: 50,
-        y: 120,
-        width: 100,
-        height: 100,
-        fill: 'blue',
-        cornerRadius: [0, 10, 20, 30],
+      let cx = width / 2;
+      let cy = height / 2;
+      let spline = new Konva.Line({
+        points: [cx, cy - 100, cx + 50, cy, cx, cy + 100],
+        stroke: 'black',
+        strokeWidth: 7,
+        tension: 0.5,
+        closed: true,
+        id: 'a',
       });
-      layer.add(rect3);
+      let spline2 = new Konva.Line({
+        points: [cx, cy - 150, cx + 50, cy, cx, cy + 100],
+        stroke: 'black',
+        strokeWidth: 7,
+        tension: 0.5,
+        closed: true,
+        id: 'a',
+      });
+
+      layer.add(spline);
+      layer.add(spline2);
 
       // add the layer to the stage
       stage.add(layer);
